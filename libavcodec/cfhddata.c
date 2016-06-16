0a512509815c3b678604ae010f3a25b1dd6063f2
Diego Biurrun <diego@biurrun.de>
2016-06-16 13:46:51 +0200

    cfhd: Drop unused TABLE_9_BAND defines

--- libavcodec/cfhddata.c
+++ libavcodec/cfhddata.c
@@ -21,14 +21,6 @@
 #include "stdint.h"
 #include "cfhd.h"
 
-/* some special codewords, not sure what they all mean */
-#define TABLE_9_BAND_END1 0x1C7859Eh
-#define TABLE_9_BAND_END_LEN1 25
-#define TABLE_9_BAND_END2 0x38F0B3Fh
-#define TABLE_9_BAND_END_LEN2 26
-#define TABLE_9_BAND_END3 0x38F0B3Eh
-#define TABLE_9_BAND_END_LEN3 26
-
 static const uint8_t table_9_vlc_len[NB_VLC_TABLE_9] = {
      1,    2,    4,    5,    5,    5,    6,    6,
      6,    7,    7,    8,    8,    8,    8,    9,
