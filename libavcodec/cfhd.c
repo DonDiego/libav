a75bc186f8a70cb55e74afcb7bb5cb2a200406de
Diego Biurrun <diego@biurrun.de>
2016-07-05 15:21:06 +0200

    cfhd: Refactor filter() a bit

--- libavcodec/cfhd.c
+++ libavcodec/cfhd.c
@@ -108,34 +108,28 @@ static inline void filter(int16_t *output, ptrdiff_t out_stride,
         if (i == 0) {
             tmp = (11 * low[0 * low_stride] - 4 * low[1 * low_stride] + low[2 * low_stride] + 4) >> 3;
             output[(2 * i + 0) * out_stride] = (tmp + high[0 * high_stride]) >> 1;
-            if (clip)
-                output[(2 * i + 0) * out_stride] = av_clip_uintp2_c(output[(2 * i + 0) * out_stride], clip);
-
-            tmp = (5 * low[0 * low_stride] + 4 * low[1 * low_stride] - low[2 * low_stride] + 4) >> 3;
-            output[(2 * i + 1) * out_stride] = (tmp - high[0 * high_stride]) >> 1;
-            if (clip)
-                output[(2 * i + 1) * out_stride] = av_clip_uintp2_c(output[(2 * i + 1) * out_stride], clip);
         } else if (i == len - 1) {
             tmp = (5 * low[i * low_stride] + 4 * low[(i - 1) * low_stride] - low[(i - 2) * low_stride] + 4) >> 3;
             output[(2 * i + 0) * out_stride] = (tmp + high[i * high_stride]) >> 1;
-            if (clip)
-                output[(2 * i + 0) * out_stride] = av_clip_uintp2_c(output[(2 * i + 0) * out_stride], clip);
-
-            tmp = (11 * low[i * low_stride] - 4 * low[(i - 1) * low_stride] + low[(i - 2) * low_stride] + 4) >> 3;
-            output[(2 * i + 1) * out_stride] = (tmp - high[i * high_stride]) >> 1;
-            if (clip)
-                output[(2 * i + 1) * out_stride] = av_clip_uintp2_c(output[(2 * i + 1) * out_stride], clip);
         } else {
             tmp = (low[(i - 1) * low_stride] - low[(i + 1) * low_stride] + 4) >> 3;
             output[(2 * i + 0) * out_stride] = (tmp + low[i * low_stride] + high[i * high_stride]) >> 1;
-            if (clip)
-                output[(2 * i + 0) * out_stride] = av_clip_uintp2_c(output[(2 * i + 0) * out_stride], clip);
+        }
+        if (clip)
+            output[(2 * i + 0) * out_stride] = av_clip_uintp2_c(output[(2 * i + 0) * out_stride], clip);
 
+        if (i == 0) {
+            tmp = (5 * low[0 * low_stride] + 4 * low[1 * low_stride] - low[2 * low_stride] + 4) >> 3;
+            output[(2 * i + 1) * out_stride] = (tmp - high[0 * high_stride]) >> 1;
+        } else if (i == len - 1) {
+            tmp = (11 * low[i * low_stride] - 4 * low[(i - 1) * low_stride] + low[(i - 2) * low_stride] + 4) >> 3;
+            output[(2 * i + 1) * out_stride] = (tmp - high[i * high_stride]) >> 1;
+        } else {
             tmp = (low[(i + 1) * low_stride] - low[(i - 1) * low_stride] + 4) >> 3;
             output[(2 * i + 1) * out_stride] = (tmp + low[i * low_stride] - high[i * high_stride]) >> 1;
-            if (clip)
-                output[(2 * i + 1) * out_stride] = av_clip_uintp2_c(output[(2 * i + 1) * out_stride], clip);
         }
+        if (clip)
+            output[(2 * i + 1) * out_stride] = av_clip_uintp2_c(output[(2 * i + 1) * out_stride], clip);
     }
 }
 
